7f4131c74b37e0fb8b246661d60f71c9f690f5a9
MarcioLSJR <32485363+MarcioLSJR@users.noreply.github.com>
2018-08-01 21:24:01 -0300

    Update Robotstest.py

--- Robotstest.py
+++ Robotstest.py
@@ -8,10 +8,10 @@ parser = urllib.robotparser.RobotFileParser()
 parser.set_url(urllib.parse.urljoin(URL_BASE, 'robots.txt'))
 parser.read()
 
-#######
+'''
 wordlist = open('wordlist.txt', 'r')
 rwordlist = wordlist.readlines()
-#######
+'''
 
 PATHS = ['/wp-admin/',
          '/2018/07/18/a-seguranca-no-wordpress/',
@@ -19,12 +19,11 @@ PATHS = ['/wp-admin/',
          ]
 
 for path in PATHS:
-    r = requests.get(url=urllib.parse.urljoin(URL_BASE, path))
+    url = urllib.parse.urljoin(URL_BASE, path)
+    r = requests.get(url)
     s = r.status_code
     if s == 200:
         print("%6s : %s" % (parser.can_fetch(AGENT_NAME, path), path))
-
-        url = urllib.parse.urljoin(URL_BASE, path)
         print("%6s : %s" % (parser.can_fetch(AGENT_NAME, url), url))
         print()
     else:
