45ab3f044c2b083a1635fe8b7e9d9fe3090ad00e
MarcioLSJR <MarcioLSJR@users.noreply.github.com>
2018-08-01 21:18:49 -0300

    Robotstest 1.0.2

--- Robotstest.py
+++ Robotstest.py
@@ -1,19 +1,31 @@
-import urllib.robotparser, urllib.parse
+import urllib.parse
+import urllib.robotparser
+import requests
 
 AGENT_NAME = '*'
-URL_BASE = 'https://hackingu.net/robots.txt'
+URL_BASE = 'https://hackingu.net/'
 parser = urllib.robotparser.RobotFileParser()
 parser.set_url(urllib.parse.urljoin(URL_BASE, 'robots.txt'))
 parser.read()
-wordlist = open('wordlist.txt','r')
+
+#######
+wordlist = open('wordlist.txt', 'r')
 rwordlist = wordlist.readlines()
+#######
+
 PATHS = ['/wp-admin/',
          '/2018/07/18/a-seguranca-no-wordpress/',
-        ]
-
+         '/123/',
+         ]
 
 for path in PATHS:
-    print("%6s : %s" % (parser.can_fetch(AGENT_NAME, path), path))
-    url = urllib.parse.urljoin(URL_BASE, path)
-    print("%6s : %s" % (parser.can_fetch(AGENT_NAME, url), url))
-    print()
+    r = requests.get(url=urllib.parse.urljoin(URL_BASE, path))
+    s = r.status_code
+    if s == 200:
+        print("%6s : %s" % (parser.can_fetch(AGENT_NAME, path), path))
+
+        url = urllib.parse.urljoin(URL_BASE, path)
+        print("%6s : %s" % (parser.can_fetch(AGENT_NAME, url), url))
+        print()
+    else:
+        print("Invalid URL")
